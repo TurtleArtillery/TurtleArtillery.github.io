3b6ffa01d0b61f1395ab148007eed020829e25b9
TurtleArtillery <38228774+TurtleArtillery@users.noreply.github.com>
2018-05-13 17:21:12 -0700

    update

--- scripts/main_mobile.js
+++ scripts/main_mobile.js
@@ -4,11 +4,11 @@
 var backdrop=document.querySelector("body");
 backdrop.addEventListener("click",function(){});
 
-var button1=document.querySelector(".circleButtonm");
-button1.addEventListener("click",function(){});
+var button1=document.querySelector(".circleButton");
+button1.addEventListener("click",function(){button1.style.backgroundColor="#FF0000";});
 
 var button2=document.querySelector(".circleButton2");
-button2.addEventListener("click",function(){alert("HEEEEEY");});
+button2.addEventListener("click",function(){});
 
 var button3=document.querySelector(".circleButton3");
 button3.addEventListener("click",function(){});
