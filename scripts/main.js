ace694240b4ec9141a88e771cc98eb35b0b7d68f
TurtleArtillery <38228774+TurtleArtillery@users.noreply.github.com>
2018-05-13 17:00:56 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 
 //Main control of scipting elements for the index.html file
 
+var button2=document.querySelector(".circleButton2");
+button2.addEventListener("click",function(){alert("HEEEEEY");});
 
 var heading=document.querySelector("h1");
 var button=document.querySelector(".circleButton4");
